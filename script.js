8ea025b2fd10148c72df1ad3abec74dfa0932deb
Suji-12 <23csec23sujithas@gmail.com>
2025-11-13 14:01:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,15 +8,49 @@ function convertToRoman(num) {
       5:['V', 5], 
       6:['I', 1]
     };
+	//your code here
+	
+	if (num === 0) return "N"; 
 
-  //your code here
+  const romanMap = [
+    ["M", 1000],
+    ["CM", 900],
+    ["D", 500],
+    ["CD", 400],
+    ["C", 100],
+    ["XC", 90],
+    ["L", 50],
+    ["XL", 40],
+    ["X", 10],
+    ["IX", 9],
+    ["V", 5],
+    ["IV", 4],
+    ["I", 1]
+  ];
+
+  let result = "";
+  
+  // handle large numbers (greater than 3999)
+  while (num >= 1000) {
+    result += "M";
+    num -= 1000;
+  }
+
+  for (let [symbol, value] of romanMap) {
+    while (num >= value) {
+      result += symbol;
+      num -= value;
+    }
+  }
+
+  return result;
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
-
-
+console.log(convertToRoman(14));
+console.log(convertToRoman(798));
+console.log(convertToRoman(36));
 
 
 // do not edit below this line
